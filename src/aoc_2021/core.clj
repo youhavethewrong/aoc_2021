87752d37d17da868ae964158fcb6e12186a3f6e1
Erick Crager <erick@youhavethewrong.info>
2021-12-01 05:47:27 -0600

    Complete Day 1 in Clojure

--- src/aoc_2021/core.clj
+++ src/aoc_2021/core.clj
@@ -16,13 +16,31 @@
                current
                (rest chart))))))
 
+(defn sliding-window-depth-count
+  [input]
+  (loop [s 0
+         last nil
+         chart input]
+    (if (or (empty? chart)
+            (< (count chart) 3))
+      s
+      (let [[a b c] (map #(Integer/parseInt %) (take 3 chart))
+            current (+ a b c)
+            next (if (and (not (nil? last))
+                          (< last current))
+                   (inc s)
+                   s)]
+        (recur next
+               current
+               (drop 1 chart))))))
+
 (defn runner
   "Read input from file and run our function."
   [f]
   (let [filename f
         content (slurp filename)
         lines (str/split content #"\n")
-        count (increasing-depth-count lines)]
+        count (sliding-window-depth-count lines)]
     count))
 
 (defn -main
