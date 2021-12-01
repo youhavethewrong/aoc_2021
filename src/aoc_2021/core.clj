b692c34df45d436996db77c74a2a3435e92cea08
Erick Crager <erick@youhavethewrong.info>
2021-12-01 05:20:41 -0600

    Complete day 1, part 1

--- src/aoc_2021/core.clj
+++ src/aoc_2021/core.clj
@@ -1,11 +1,30 @@
 (ns aoc-2021.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn increasing-depth-count
+  [input]
+  (loop [c 0
+         last nil
+         chart input]
+    (if (empty? chart)
+      c
+      (let [current (Integer/parseInt (first chart))]
+        (recur (if (or (nil? last) (< current last))
+                 c
+                 (inc c))
+               current
+               (rest chart))))))
+
+(defn runner
+  "Read input from file and run our function."
+  [f]
+  (let [filename f
+        content (slurp filename)
+        lines (str/split content #"\n")
+        count (increasing-depth-count lines)]
+    count))
 
 (defn -main
   [& args]
-  (foo (first args)))
+  (runner (first args)))
